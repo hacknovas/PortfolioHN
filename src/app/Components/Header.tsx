54e9fa56540c80f7dbce6fcda299aabe7f9c3028
hacknovas <creatives.doni@gmail.com>
2024-02-09 21:27:48 +0530

    adding css

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import "@/app/CSS/header.css";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="" id="setImage">
+      <div id="imageset" ></div>
+      <div className="text-light d-flex justify-content-between ">
+        <div className="p-3">Prathamesh Doni</div>
+        <div className="d-flex">
+          <div className="p-3">About</div>
+          <div className="p-3">Skills</div>
+          <div className="p-3">Projects</div>
+          <div className="p-3">Qualification</div>
+        </div>
+      </div>
+    </div>
+  );
 }
