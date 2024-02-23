ba007909c00dbac3ed52c572b8fd51ef7e9b65f7
hacknovas <creatives.doni@gmail.com>
2024-02-23 15:07:52 +0530

    skills  section updated

--- src/app/Components/Skills.tsx
+++ src/app/Components/Skills.tsx
@@ -1,6 +1,5 @@
 "use client"
 import styles from "@/app/CSS/skill.module.css"
-import Image from "next/image"
 import { useState } from "react"
 
 export default function Skills() {
@@ -16,35 +15,47 @@ export default function Skills() {
                 <div className={styles.skilllist} >
                     <div className={styles.sectionName}>Skills</div>
                     <div >
-                        <div className={styles.skilllistItem}>Web 2.0</div>
-                        <div className={styles.skilllistItem}>Web 3.0</div>
-                        <div className={styles.skilllistItem}>Tools</div>
+                        <div className={styles.skilllistItem} onClick={() => {
+                            document.querySelector<HTMLElement>("#icon1 ")!.style.display = "flex";
+                            document.querySelector<HTMLElement>("#icon2 ")!.style.display = "none";
+                            document.querySelector<HTMLElement>("#icon3 ")!.style.display = "none";
+                        }}>Web 2.0</div>
+                        <div className={styles.skilllistItem} onClick={() => {
+                            document.querySelector<HTMLElement>("#icon2")!.style.display = "flex";
+                            document.querySelector<HTMLElement>("#icon3")!.style.display = "none";
+                            document.querySelector<HTMLElement>("#icon1")!.style.display = "none";
+                        }}>Web 3.0</div>
+                        <div className={styles.skilllistItem} onClick={() => {
+                            document.querySelector<HTMLElement>("#icon3")!.style.display = "flex";
+                            document.querySelector<HTMLElement>("#icon2")!.style.display = "none";
+                            document.querySelector<HTMLElement>("#icon1")!.style.display = "none";
+                        }}>Tools</div>
                     </div>
                 </div>
-                <div>
-                    <div className={styles.iconsList}>
+                <div className={styles.allIconsList}>
+                    <div id="icon1" className={styles.iconsList1}>
                         {
-                            web2.map((icons: any) => {
+                            web2.map((icons: any, i) => {
                                 return (
-                                    <i className={`devicon-${icons}-plain colored`}></i>
+                                    <i className={`devicon-${icons}-plain colored`} key={i}></i>
                                 )
                             })
                         }
                     </div>
-                    <div className={styles.iconsList}>
+                    <div id="icon2" className={styles.iconsList2}>
                         {
-                            web3.map((icons: any) => {
+                            web3.map((icons: any, i) => {
                                 return (
-                                    <i className={`devicon-${icons}-plain colored`}></i>
+                                    <i className={`devicon-${icons}-plain colored`} key={i}></i>
                                 )
                             })
                         }
                     </div>
-                    <div className={styles.iconsList}>
+                    <div className={styles.iconsList3} id="icon3">
                         {
-                            tools.map((icons: any) => {
+                            tools.map((icons: any, i) => {
                                 return (
-                                    <i className={`devicon-${icons}-plain colored`}></i>
+                                    <i className={`devicon-${icons}-plain colored`} key={i}></i>
                                 )
                             })
                         }
