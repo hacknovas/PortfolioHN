0168c8024e9d33dba355ab65fd62a968c07be04a
hacknovas <creatives.doni@gmail.com>
2024-02-24 13:02:48 +0530

    updated project style

--- src/app/Components/Project.tsx
+++ src/app/Components/Project.tsx
@@ -74,7 +74,7 @@ export default function Project() {
                     }
                 </div>
                 <div className={styles.ScrollButton}>
-                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAc5JREFUSEu11UuoTVEcx/HPHaFQlEcpDJAMyFSKifLIwDW4SphIJFLKyDMzZSBSMmOgDHS7dVPqDjxHBmYeSQYmykAp5W3/a5/slrX2PufW2bOzzvr/v//nb40Y8jcyZP+6ADOwG1uxDsvxB+/xAvdxDz9KgbYBtuF67bQt0bc4jKncpRLgfBXpuQHK9xuncDm1yQEu4OwAzptXT+BK8yAF7MREwfklHMCiFnhksglPeneagDl4g8UZB9dwDCvxqHCnZ/a6avxafI+DJuBI3dRcgB+xAe+wAs+woCWTXRhPAZPY3mLUhKyuMylBbuJQCviAJR3NDUjUOMrQBokdWZ8CvmJWH9PzCRsbkKeYn9h9rqZpXgr4gtnTAETT01KFr7kpINJe1QFIo885DxcvsSYF3MGejib3U/9wcbuauP0pYB9uFQCDTFC4GKsyuJsCosGvsDQDCdE7Wk/Ow2pfFrZkGssai/YtBcTvLdVCPSgYh5Dt7djikIrNeJyTit7ZaVzsY5pyV47javOPklyfqWQkVLXrQer5+oWTqZLmStSE78CNPrY7HpyDiN7893VFOBOjtUZF45bVT2aI3vO6XyFqP0sl7QJMsxX/zIYO+AtJ71YZvcQVbwAAAABJRU5ErkJggg==" />
+                    <i className='bx bxs-chevron-right-circle'></i>
                 </div>
                 {/* <i className="ci ci-spotify ci-2x"></i> */}
             </div>
