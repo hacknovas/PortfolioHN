5d5e2072070073a98113219d6cce0d3560663c74
hacknovas <creatives.doni@gmail.com>
2024-02-28 00:33:27 +0530

    project button checked

--- src/app/Components/Project.tsx
+++ src/app/Components/Project.tsx
@@ -6,7 +6,7 @@ import { Key, useEffect, useState } from "react";
 import projectJSON from "@/utils/mydata.myprojects.json"
 
 export default function Project() {
-
+    const [tag, settag] = useState<string>("Personal")
     type projectS = {
         _id: {
             $oid: string
@@ -21,8 +21,8 @@ export default function Project() {
 
     const [projects, setprojects] = useState<projectS[]>();
 
+
     function loadFun() {
-        console.log(projectJSON);
         setprojects(projectJSON)
     }
 
@@ -37,38 +37,43 @@ export default function Project() {
             <div className={styles.projectSectionDiv}>
                 <div className={styles.sectionName}>Project Showcase</div>
                 <div className={styles.projectType}>
-                    <div >Personal</div>
-                    <div>Academic</div>
+                    <div onClick={() => {
+                        settag("Personal");
+                    }}>Personal</div>
+                    <div onClick={() => {
+                        settag("Academic");
+                    }}>Academic</div>
                 </div>
                 <div className={styles.projectItems}>
                     {
                         projects?.map((_data: any, i) => {
                             return (
-                                <div className={styles.projectSingleItem} key={i}>
-                                    <div>
-                                        <b>{_data.Name}</b>
-                                    </div>
-                                    <div>
-                                        <Image src={_data.Image} alt="Na" width={300} height={150} ></Image>
-                                    </div>
-                                    <div style={{ textAlign: "center", padding: "2% 0" }}>
-                                        {
-                                            _data.Tools.map((icons: any, j: Key | null | undefined) => {
-                                                return (
-                                                    <i className={`devicon-${icons}-plain colored`} key={j}></i>
-                                                )
-                                            })
-                                        }
-                                    </div>
-                                    <div style={{ opacity: "0.7" }}>
-                                        {_data.Description.substr(0, 90) + "..."}
-                                    </div>
-                                    <div>
-                                        <a href={_data.GithubLink} target="_blank" style={{ textDecoration: "none", color: "white" }}>
-                                            <i className={`devicon-${"github"}-plain colored`}></i>
-                                        </a>
-                                    </div>
-                                </div>
+                                _data.tag == tag ?
+                                    <div className={styles.projectSingleItem} key={i}>
+                                        <div>
+                                            <b>{_data.Name}</b>
+                                        </div>
+                                        <div>
+                                            <Image src={_data.Image} alt="Na" width={300} height={150} ></Image>
+                                        </div>
+                                        <div style={{ textAlign: "center", padding: "2% 0" }}>
+                                            {
+                                                _data.Tools.map((icons: any, j: Key | null | undefined) => {
+                                                    return (
+                                                        <i className={`devicon-${icons}-plain colored`} key={j}></i>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                        <div style={{ opacity: "0.7" }}>
+                                            {_data.Description.substr(0, 90) + "..."}
+                                        </div>
+                                        <div>
+                                            <a href={_data.GithubLink} target="_blank" style={{ textDecoration: "none", color: "white" }}>
+                                                <i className={`devicon-${"github"}-plain colored`}></i>
+                                            </a>
+                                        </div>
+                                    </div> : null
                             )
                         })
                     }
