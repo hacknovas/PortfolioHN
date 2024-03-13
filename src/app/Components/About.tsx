5234a336aa97740977b9524a5c3edf4a4ccb55ef
hacknovas <creatives.doni@gmail.com>
2024-03-13 14:34:07 +0530

    Updated about section

--- src/app/Components/About.tsx
+++ src/app/Components/About.tsx
@@ -1,5 +1,6 @@
 
 import styles from "@/app/CSS/about.module.css";
+import SlideTemplate from "./Animate/SlideTemplate";
 
 export default function About() {
     return (
@@ -7,13 +8,15 @@ export default function About() {
             <div className={styles.sectionIntro}>Who am I? </div>
             <div className={styles.aboutSection}>
                 <div className={styles.aboutSLeft}>
-                    <div className={styles.aboutText} >
-                        <div className={styles.sectionName}>About</div>
-                        I'm <br /> Full Stack Developer <br /> primarily Working On Web 2.0 and Blockhain Technology.
-                    </div>
-                    <p>
-                        As a lifelong learner, I'm driven to excel in any tech field<br />And constantly expanding my skills.
-                    </p>
+                    <SlideTemplate>
+                        <div className={styles.aboutText} >
+                            <div className={styles.sectionName}>About</div>
+                            I'm <br /> Full Stack Developer <br /> primarily Working On Web 2.0 and Blockhain Technology.
+                        </div>
+                        <p>
+                            As a lifelong learner, I'm driven to excel in any tech field<br />And constantly expanding my skills.
+                        </p>
+                    </SlideTemplate>
                 </div>
                 <div className={styles.aboutSReft}>
                     imageNA
