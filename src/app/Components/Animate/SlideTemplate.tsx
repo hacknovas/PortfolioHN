ff8056ce4d4f4b273374f6a7a24aec46688dd201
hacknovas <creatives.doni@gmail.com>
2024-03-13 14:02:40 +0530

    Updated with slides animation

--- src/app/Components/Animate/SlideTemplate.tsx
+++ src/app/Components/Animate/SlideTemplate.tsx
@@ -8,11 +8,11 @@ export default function Template({ children }: { children: React.ReactNode }) {
         once: true
     })
 
-    const mainControls = useAnimation()
+    const slideControls = useAnimation()
 
     useEffect(() => {
         if (isInView) {
-            mainControls.start("visible")
+            slideControls.start("visible")
         }
     }, [isInView])
 
@@ -20,15 +20,23 @@ export default function Template({ children }: { children: React.ReactNode }) {
         <div ref={ref} style={{ position: "relative", overflow: "hidden" }}>
             <motion.div
                 variants={{
-                    hidden: { opacity: 0, y: -50 },
-                    visible: { opacity: 1, y: 0 },
+                    hidden: { left: 0 },
+                    visible: { left: "100%" },
                 }}
+
                 initial="hidden"
-                animate={mainControls}
-                transition={{ duration: 0.5, delay: 0.25 }}
+                animate={slideControls}
+                transition={{ duration: 0.5, ease: "easeIn" }}
+                style={{
+                    position: "absolute",
+                    top: 4,
+                    bottom: 4,
+                    left: 0,
+                    right: 0, background: "var(--brand)",
+                    zIndex: 20
+                }}
             >
                 {children}
-
             </motion.div>
         </div>
     )
