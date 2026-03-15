484f175cc523c4fdd8ef5cd742e51d7d9b72851f
Prathamesh Doni <creatives.doni@gmail.com>
2026-03-15 20:23:30 +0530

    Updated Experience Section

--- src/app/Components/Experience.tsx
+++ src/app/Components/Experience.tsx
@@ -12,6 +12,12 @@ export default function Experience() {
                         <h3>Associate Software Engineer</h3>
                         <div>Capgemini, India</div>
                         <p><i className='bx bx-map'></i> Pune, Maharashtra, India</p>
+                        <ul>
+                            <li>Working as a Full Stack Developer for Aptiv, a global automotive technology company.</li>
+                            <li>Developing and maintaining enterprise applications using C#, VB.NET, and SQL-based databases (SQL Server, MySQL, PL/SQL).</li>
+                            <li>Managing and supporting multiple Supply Chain Management (SCM) applications to ensure smooth logistics operations and 24/7 system availability.</li>
+                            <li>Collaborating with business stakeholders to understand requirements and deliver effective technical solutions.</li>
+                        </ul>
                     </div>
 
                     <div className={styles.singleEMPTYL}></div>
@@ -22,6 +28,9 @@ export default function Experience() {
                         <h3>Full Stack Developer </h3>
                         <div>Edunet Foundation</div>
                         <p><i className='bx bx-map'></i> Remote</p>
+                        <ul>
+                            <li>Developed a product comparison website, managing the full lifecycle from data collection and preprocessing to generating user-focused comparison results.</li>
+                        </ul>
                     </div>
                 </div>
             </div>
